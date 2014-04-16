88c88a3d4da78b4025876a0ed6bb58664dd44142
gsalisi <geoffreysalisi@gmail.com>
2014-04-16 14:06:50 -0400

    Added dynamic timing to egg fall by implementing random delays and durations.

--- src/ca/gsalisi/games/eggs/EggGame.java
+++ src/ca/gsalisi/games/eggs/EggGame.java
@@ -32,6 +32,8 @@ public class EggGame {
 	private Animation eggAnimation;
 	private int eggDelayTime;
 	private boolean animationStarted;
+	private Handler eggDelayHandler;
+	private Runnable eggRunnable;
 	
 	protected int level;
 	protected int scoreCount;
@@ -66,11 +68,11 @@ public class EggGame {
 
 			@Override
 			public void run() {
-				main.hScroll.smoothScrollTo(main.convertToPixel(120),0);
-				
+				main.hScroll.smoothScrollTo(main.convertToPixel(120),0);	
 			}
 			
 		});
+		
 		// creates a delay before the start of the game
 		main.countdownView.setVisibility(View.VISIBLE);
 		main.reset_btn.setEnabled(false);
@@ -108,7 +110,7 @@ public class EggGame {
 		//sensorManager.registerListener(eventListener, rtnVectorSensor, 50000);
 
 		level = 0;
-		eggDelayTime = 3500;
+		eggDelayTime = 3000;
 
 		final Handler handler = new Handler();
 		TimerTask levelTimerTask = new TimerTask() {
@@ -120,7 +122,7 @@ public class EggGame {
 					@Override
 					public void run() {
 						if(timerRunning){
-							if (level != 0 && eggDelayTime >= 500) {
+							if (level != 0 && eggDelayTime >= 800) {
 								eggDelayTimer.cancel();
 								eggDelayTimer.purge();
 								eggDelayTime -= 100;
@@ -140,8 +142,7 @@ public class EggGame {
 		masterLvlTimer = new Timer();
 		masterLvlTimer.schedule(levelTimerTask, 0, 10000);
 		
-		//set things that prevent crash 
-		//when game is stopped while counting down
+		//enable reset button when the timers are running 
 		main.reset_btn.setEnabled(true);
 		timerRunning = true; 
 	}
@@ -172,8 +173,6 @@ public class EggGame {
 
 	}// end startEggFallTimer
 
-
-
 	// Generates a random position for the egg fall
 	protected int generateRandomPosition() {
 		Random rand = new Random();
@@ -181,42 +180,68 @@ public class EggGame {
 
 	}// end generateRandomPosition
 
+	//method that creates and animates the egg
 	protected void startEggFall(int position) {
-
+		//set to final so it's accessible inside runnable
 		final int pos = position;
-
-		final ImageView eggView = main.createEgg(pos);
 		
-		eggAnimation = AnimationUtils.loadAnimation(main,
-				R.anim.eggdrop);
-
-		eggView.startAnimation(eggAnimation);
-		animationStarted = true;
-		animListener = new AnimationListener() {
-
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				if(timerRunning){
-					eggView.setVisibility(View.GONE);
-					main.checkIfScored(pos);
-				}
-			}
+		//create a random delay form 0 to 800 milliseconds 
+		//for every egg fall
+		Random r = new Random();
+		int delayEggFall = r.nextInt(800);
+		
+		eggDelayHandler = new Handler();
+		eggRunnable = new Runnable(){
 
 			@Override
-			public void onAnimationRepeat(Animation animation) {
+			public void run() {
+				//creates the egg
+				final ImageView eggView = main.createEgg(pos);
+				
+				eggAnimation = AnimationUtils.loadAnimation(main,
+						R.anim.eggdrop);
+				//set a random duration for egg fall ranging from 
+				//1.6-2 seconds
+				Random rand = new Random();
+				int duration = rand.nextInt(400) + 1600;
+				
+				eggAnimation.setDuration(duration);
+				
+				eggView.startAnimation(eggAnimation);
+				animationStarted = true;
+				animListener = new AnimationListener() {
 
+					@Override
+					public void onAnimationEnd(Animation animation) {
+						if(timerRunning){
+							eggView.setVisibility(View.GONE);
+							main.checkIfScored(pos);
+						}
+					}
+
+					@Override
+					public void onAnimationRepeat(Animation animation) {
+
+					}
+
+					@Override
+					public void onAnimationStart(Animation animation) {
+
+					}
+
+				};
+				//set the listener
+				eggAnimation.setAnimationListener(animListener);
+						
 			}
-
-			@Override
-			public void onAnimationStart(Animation animation) {
-
-			}
-
+			
 		};
-		eggAnimation.setAnimationListener(animListener);
+		eggDelayHandler.postDelayed(eggRunnable, delayEggFall);
+			
 
 	}// end startEggFall()
 
+	//restarts the game
 	public void resetGame() {
 		Log.d("resetGame", "RESET");
 		numberOfLives = 3;
@@ -225,13 +250,18 @@ public class EggGame {
 		main.updateScore(scoreCount);
 		
 		startGame();
-	}
+		
+	}//end resetGame();
 
+	//stops the game
 	public void stopGame() {
 		Log.d("stopGame", "STOPPED");
-		//important! this cancels remaining task scheduled on master timer
-		level = 0; 
+		
+		level = 0; //important! this prevents remaining task scheduled 
+					//on master timer and level timer to continue 
+		
 		cancelTimers();
+		
 		if(animationStarted){
 			eggAnimation.cancel();
 			main.eggView.clearAnimation();
@@ -239,10 +269,14 @@ public class EggGame {
 		//sensorManager.unregisterListener(eventListener);
 		startedGame = false;
 		animationStarted = false;
-	}
+		
+	}//end of stopGame()
 
+	//cancels timers
 	void cancelTimers() {
 		Log.d("cancelTimer", "TIMER CANCELLED");
+		eggDelayHandler.removeCallbacks(eggRunnable);
+		
 		timerRunning = false;
 		
 		masterLvlTimer.cancel();
@@ -250,6 +284,6 @@ public class EggGame {
 		
 		masterLvlTimer.purge();
 		eggDelayTimer.purge();
-	}
+	}//end of cancelTimers()
 	
 }
